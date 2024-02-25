836ff51e10f8c563c74a2140e65ec0c40e0ace2d
Julian Bourne <julian@britesc.com>
2024-02-25 22:28:52 +0100

    Created project_pins.h

---
+++ project_pins.h
@@ -0,0 +1,100 @@
+/* Copyright 2024 Julian Bourne
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef project_pins_h_
+	#define project_pins_h_
+
+	"""
+	Pico W Pin Numbers.
+
+	These numbers correspond to the Silk Screen Numbering.
+	"""
+
+	#define _PicoW_Pin01 (00u)
+        #define _PicoW_Pin02 (01u)
+        #define _PicoW_Pin03 (02u)
+        #define _PicoW_Pin04 (03u)
+        #define _PicoW_Pin05 (04u)
+        #define _PicoW_Pin06 (05u)
+        #define _PicoW_Pin07 (06u)
+        #define _PicoW_Pin08 (07u)
+        #define _PicoW_Pin09 (08u)
+        #define _PicoW_Pin10 (09u)
+        #define _PicoW_Pin11 (10u)
+        #define _PicoW_Pin12 (11u)
+        #define _PicoW_Pin13 (12u)
+        #define _PicoW_Pin14 (13u)
+        #define _PicoW_Pin15 (14u)
+        #define _PicoW_Pin16 (15u)
+        #define _PicoW_Pin17 (16u)
+        #define _PicoW_Pin18 (17u)
+        #define _PicoW_Pin19 (18u)
+        #define _PicoW_Pin20 (19u)
+        #define _PicoW_Pin21 (20u)
+        #define _PicoW_Pin22 (21u)
+        #define _PicoW_Pin23 (22u)
+        #define _PicoW_Pin24 (23u)
+        #define _PicoW_Pin25 (24u)
+        #define _PicoW_Pin26 (25u)
+        #define _PicoW_Pin27 (26u)
+        #define _PicoW_Pin28 (27u)
+        #define _PicoW_Pin29 (28u)
+        #define _PicoW_Pin30 (29u)
+        #define _PicoW_Pin31 (30u)
+        #define _PicoW_Pin32 (31u)
+        #define _PicoW_Pin33 (32u)
+        #define _PicoW_Pin34 (33u)
+        #define _PicoW_Pin35 (34u)
+        #define _PicoW_Pin36 (35u)
+        #define _PicoW_Pin37 (36u)
+        #define _PicoW_Pin38 (37u)
+        #define _PicoW_Pin39 (38u)
+        #define _PicoW_Pin40 (39u)
+
+	"""
+	Hub75 Pin Numbers.
+
+	These numbers correspond to the Silk Screen Numbering.
+	"""
+
+        #define _PicoW_Hub75_R1   (00u)
+        #define _PicoW_Hub75_G1   (00u)
+        #define _PicoW_Hub75_B1   (00u)
+        #define _PicoW_Hub75_GND1 (00u)
+        #define _PicoW_Hub75_R2   (00u)
+        #define _PicoW_Hub75_G2   (00u)
+        #define _PicoW_Hub75_B2   (00u)
+        #define _PicoW_Hub75_E    (00u)
+        #define _PicoW_Hub75_A    (00u)
+        #define _PicoW_Hub75_B    (00u)
+        #define _PicoW_Hub75_C    (00u)
+        #define _PicoW_Hub75_D    (00u)
+        #define _PicoW_Hub75_CLK  (00u)
+        #define _PicoW_Hub75_LAT  (00u)
+        #define _PicoW_Hub75_OE   (00u)
+        #define _PicoW_Hub75_GND2 (00u)
+
+
+
+
+
+
+
+#endif
